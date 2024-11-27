4660a5a2179d9e9714247fc0acc005fe92aebc96
Yekaditya Devadi <141273781+yekaditya15@users.noreply.github.com>
2024-11-27 19:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create the student object
+const student = {
+    name: "John Doe",
+};
+
+// Add getKeys method to Object.prototype
+Object.prototype.getKeys = function () {
+    return Object.keys(this);
+};
+
+// Exporting student for potential further usage
+window.student = student;
